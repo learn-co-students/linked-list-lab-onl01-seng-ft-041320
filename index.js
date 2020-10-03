25600927fae3c941e70c8c63bd5ae4fd5e1fcacd
mattenbar <matthw.enbar@gmail.com>
2020-10-03 09:47:50 -0400

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getName(node){
 function headNode(linkedList, collection){
   return collection[linkedList]
 }
+
+function next(node, collection){
+  
+  let nextAddress = node.next
+  
+  return collection[`${nextAddress}`]
+}
