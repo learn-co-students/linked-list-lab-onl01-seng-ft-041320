7a42b3aa2e0aef77f028d0d1a94b02063d877b39
ReginaF2012 <skritschy@gmail.com>
2020-12-23 09:46:57 -0600

    Done.

--- index.js
+++ index.js
@@ -1,54 +1,57 @@
-function getName(node){
-    return node.name;
-};
+function getName(node) {
+  return node.name;
+}
 
-function headNode(head, collection){
-    return collection[head];
-};
+function headNode(head, collection) {
+  return collection[head];
+}
 
-function next(node, collection){
-    let address = node.next;
-    return collection[address];
-};
+function next(node, collection) {
+  let address = node.next;
+  return collection[address];
+}
 
-function nodeAt(index, head, collection){
-    let node = collection[head];
-    for(let i = 0; i < index; i++){
-        node = next(node, collection);
+function nodeAt(index, head, collection) {
+  let node = collection[head];
+  for (let i = 0; i < index; i++) {
+    node = next(node, collection);
+  }
+  return node;
+}
+
+function addressAt(index, head, collection) {
+  if (index === 0) {
+    return head;
+  } else {
+    let node = nodeAt(index - 1, head, collection);
+    return node.next;
+  }
+}
+
+function indexAt(node, collection, head) {
+  let currentNode = collection[head];
+  let index = 0;
+  while (currentNode) {
+    if (currentNode === node) {
+      return index;
     }
-    return node;
-};
+    currentNode = next(currentNode, collection);
+    index++;
+  }
+  return false;
+}
 
-function addressAt(index, head, collection){
-    if(index === 0){
-      return head;
-    } else {
-      let node = nodeAt(index - 1, head, collection);
-      return node.next;
-    }
-};
+function insertNodeAt(index, address, head, collection) {
+  let previousNode = nodeAt(index - 1, head, collection);
+  let nextAddress = previousNode.next;
 
-function indexAt(node, collection, head){
-    let currentNode = collection[head];
-    let index = 0;
-    while(currentNode !== node){
-      currentNode = next(currentNode, collection);
-      index++;
-    }
-    return index;
-};
+  previousNode.next = address;
+  collection[address].next = nextAddress;
+}
 
-function insertNodeAt(index, address, head, collection){
-    let previousNode = nodeAt(index - 1, head, collection);
-    let nextAddress = addressAt(index, head, collection);
+function deleteNodeAt(index, head, collection) {
+  let previousNode = nodeAt(index - 1, head, collection);
+  let nextAddress = addressAt(index + 1, head, collection);
 
-    previousNode.next = address;
-    collection[address].next = nextAddress;
-};
-
-function deleteNodeAt(index, head, collection){
-    let previousNode = nodeAt(index - 1, head, collection);
-    let nextAddress = addressAt(index + 1, head, collection);
-
-    previousNode.next = nextAddress;
+  previousNode.next = nextAddress;
 }
